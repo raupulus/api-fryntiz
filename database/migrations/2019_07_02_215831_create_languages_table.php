1bbcc45844182cc689e4fcc649d0f3184465e1ac
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-07-03 00:37:25 +0200

    Creo migración para la tabla de lenguajes

---
+++ database/migrations/2019_07_02_215831_create_languages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->bigIncrements('id');
+            $table->char('code', 8);
+            $table->char('code2', 2);
+            $table->string('name', 200);
+            $table->string('country', 255);
+            $table->string('icon16', 512);
+            $table->string('icon32', 512);
+            $table->string('icon64', 512);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('languages');
+    }
+}
