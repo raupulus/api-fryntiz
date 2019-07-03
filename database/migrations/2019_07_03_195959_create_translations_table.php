75100ca9de3d7128fcd1e11f2a190272a598707f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-07-03 22:17:24 +0200

    Creo migración para la tabla de de traducciones: translations

---
+++ database/migrations/2019_07_03_195959_create_translations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->bigIncrements('id');
+            $table->foreign('language_id')
+                  ->references('id')->on('languages')
+                  ->onUpdate('cascade')
+                  ->onDelete('delete');
+            $table->string('token', 512);
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
