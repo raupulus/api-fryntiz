80e0324393338f6eb9f49f1ef42ec0e8072c78ab
Raúl Caro Pastorino <dev@fryntiz.es>
2019-07-08 07:16:34 +0200

    Completo migración para Formación académica complementaria: academy complementary

---
+++ database/migrations/2019_07_08_050854_create_academic_complementary_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcademicComplementaryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+         * id - image_id - translation_name_token -
+         * translation_description_token - entity - hours
+         */
+        Schema::create('academic_complementary', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('image_id');
+            $table->foreign('image_id')
+                ->references('id')->on('files')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+            $table->string('translation_name_token', 511);
+            $table->foreign('translation_name_token')
+                ->references('token')->on('translations')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+            $table->string('translation_description_token', 511);
+            $table->foreign('translation_description_token')
+                ->references('token')->on('translations')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+            $table->string('entity', 511);
+            $table->integer('hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academic_complementary', function (Blueprint $table) {
+            $table->dropForeign(['image_id']);
+            $table->dropForeign(['translation_name_token']);
+            $table->dropForeign(['translation_rank_token']);
+            $table->dropForeign(['translation_description_token']);
+        });
+    }
+}
