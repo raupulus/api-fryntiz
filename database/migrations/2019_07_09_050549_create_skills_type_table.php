10651b7dc81ca7fd20b5c5beac289f2d8522b9c4
Raúl Caro Pastorino <dev@fryntiz.es>
2019-07-09 07:13:11 +0200

    Creo migración para la tabla de tipos de habilidades: skills_type

---
+++ database/migrations/2019_07_09_050549_create_skills_type_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillsTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // id - image_id - translation_name_token - translation_description_token
+        Schema::create('skills_type', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('image_id');
+            $table->foreign('image_id')
+                ->references('id')->on('files')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+            $table->string('translation_name_token', 511);
+            $table->foreign('translation_name_token')
+                ->references('token')->on('translations')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+            $table->string('translation_description_token', 511);
+            $table->foreign('translation_description_token')
+                ->references('token')->on('translations')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills_type', function (Blueprint $table) {
+            $table->dropForeign(['image_id']);
+            $table->dropForeign(['translation_name_token']);
+            $table->dropForeign(['translation_description_token']);
+        });
+    }
+}
