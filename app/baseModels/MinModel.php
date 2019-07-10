9d3729dbe74cfe4c4d04a325ddbe21f0b3565388
Raúl Caro Pastorino <dev@fryntiz.es>
2019-07-10 07:09:28 +0200

    Añado directorio para modelos básicos de los que extender partes comunes

---
+++ app/baseModels/MinModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class MinModel
+ * Modelo mínimo con funciones comunes a todos los modelos.
+ *
+ * @package App
+ */
+class MinModel extends Model
+{
+
+}
---
+++ app/baseModels/README.md
@@ -0,0 +1,2 @@
+Directorio con los modelos base desde los que heredar para reunir así la 
+parte común de varias clases.
