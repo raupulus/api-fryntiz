4779be6bdac9327e7dcb99e767b98178de84dcd4
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-07-22 22:59:34 +0200

    Añado información para el despliegue, mejoro configuración de apache y muestro algunos datos en la página principal.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,108 +3,74 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Api Fryntiz</title>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        @php
+            $datos = [
+                'Humedad' => \App\Humidity::all(),
+                'Temperatura' => \App\Temperature::all(),
+                'Presión' => \App\Pressure::all(),
+            ];
+        @endphp
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1>API - Modo depuración temporalmente</h1>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                    <p>
+                        Datos no reales/fiables
+                    </p>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <p>
+                        Esta API se encuentra en construcción.
+                    </p>
+
+                    <p>
+                        Puedes ver el desarrollo aquí:
+                        <a href="https://gitlab.com/fryntiz/api-fryntiz/tree/master"
+                           title="Api fryntiz en GitLab">
+                            https://gitlab.com/fryntiz/api-fryntiz
+                        </a>
+                    </p>
                 </div>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-12">
-                <h3>Testeando</h3>
-            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    @foreach($datos as $title => $collection)
+                        <h2>{{$title}}</h2>
+                        <table class="table table-bordered table-dark">
+                            <head>
+                                <tr>
+                                    <td>
+                                        Timestamp
+                                    </td>
 
-            <div class="col-md-12">
-                @foreach(\App\User::all() as $user)
-                    {{$user->name}}
-                @endforeach
+                                    <td>
+                                        Valor
+                                    </td>
+                                </tr>
+                            </head>
+                            <tbody>
+                                @foreach($collection as $ele)
+                                    <tr>
+                                        <td>
+                                            {{$ele->created_at}}
+                                        </td>
+
+                                        <td>
+                                            {{$ele->value}}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endforeach
+                </div>
             </div>
         </div>
     </body>
