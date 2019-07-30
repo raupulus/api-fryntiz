7eeb069e63301d3cd720822961ffeb7b3a3ff051
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-07-31 00:21:30 +0200

    Corrijo ruta

--- routes/webhook.php
+++ routes/webhook.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
 
-Route::group(['prefix' => 'webhook'], function () {
+Route::group([], function () {
     Route::post('/api-deploy', '\Webhook\GitlabWebhook@apiDeploy');
     Route::post('/api-notification', '\Webhook\GitlabWebhook@apiNotification');
 });
