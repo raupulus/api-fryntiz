a6972a74015a148d8a5725af02a2dce54cd9e270
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-09-24 14:28:10 +0200

    Añado barra de navegación superior enlazando a cada sección de datos y ahora muestra rayos uv

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <title>Api Fryntiz</title>
     </head>
 
-    <body>
+    <body data-spy="scroll" data-target=".navbar" data-offset="50">
         @php
             $datos = [
                 'Humedad' => \App\Humidity::whereNotNull('value')
@@ -22,10 +22,33 @@
                 'Luz' => \App\Light::whereNotNull('value')
                     ->orderBy('created_at', 'DESC')
                     ->paginate(20),
+                'Rayos UV' => \App\Uv::whereNotNull('uv_raw')
+                    ->orderBy('created_at', 'DESC')
+                    ->paginate(20),
             ];
         @endphp
 
-        <div class="container">
+        {{-- Navbar --}}
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="{{url('/')}}">API | WS</a>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    @foreach(array_keys($datos) as $titulo)
+                        <li class="nav-item">
+                            <a href="#{{$titulo}}" class="nav-link">
+                                {{$titulo}}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container mt-5">
             <div class="row">
                 <div class="col-md-12 text-center">
                     <h1>API - Modo depuración temporalmente</h1>
@@ -33,6 +56,23 @@
                     <p>
                         Datos no fiables durante el desarrollo, franja horaria
                         UTC
+                    </p>
+
+                    <p class="alert alert-danger text-left">
+                        <span class="text-danger ">
+                            Esta página es
+                            <strong>temporal</strong>
+                            solo para detectar posibles caídas/cuelgues en los
+                            programas que componen tanto sensores, arduinos,
+                            raspberrys, servidores... etc
+                            <br />
+                            Una vez acabada la aplicación desaparecerá quedando
+                            en el subdominio:
+                            <a href="https://eltiempo.desdechipiona.es"
+                               title="El tiempo Desde Chipiona">
+                                https://eltiempo.desdechipiona.es
+                            </a>
+                        </span>
                     </p>
 
                     <p>
@@ -64,9 +104,13 @@
             <div class="row">
                 <div class="col-md-12">
                     @foreach($datos as $title => $collection)
-                        <h2>
+                        @php
+                            $att_fillables = $collection->get(0)->getFillable();
+                        @endphp
+                        <h2 id="{{$title}}">
                             {{$title}} -
                             <small>
+                                Viendo últimos
                                 <strong>{{$collection->count()}}</strong>
                                 registros
                             </small>
@@ -84,7 +128,15 @@
                                     <h5 class="card-title"></h5>
                                     <p class="card-text text-center">
                                         <strong>
-                                            {{$collection->max('value')}}
+                                            @if($collection->count() >= 1)
+                                                @if(in_array('value',$att_fillables))
+                                                    {{$collection->max('value')}}
+                                                @elseif (in_array('uv_raw',$att_fillables))
+                                                    {{$collection->max('uv_raw')}}
+                                                @elseif (in_array('average',$att_fillables))
+                                                    {{$collection->max('average')}}
+                                                @endif
+                                            @endif
                                         </strong>
                                     </p>
                                 </div>
@@ -101,7 +153,15 @@
                                     <h5 class="card-title"></h5>
                                     <p class="card-text text-center">
                                         <strong>
-                                            {{$collection->min('value')}}
+                                            @if($collection->count() >= 1)
+                                                @if(in_array('value',$att_fillables))
+                                                    {{$collection->min('value')}}
+                                                @elseif (in_array('uv_raw',$att_fillables))
+                                                    {{$collection->min('uv_raw')}}
+                                                @elseif (in_array('average',$att_fillables))
+                                                    {{$collection->min('average')}}
+                                                @endif
+                                            @endif
                                         </strong>
                                     </p>
                                 </div>
@@ -118,12 +178,15 @@
                                     <h5 class="card-title"></h5>
                                     <p class="card-text text-center">
                                         <strong>
-                                            @php
-                                                $max = $collection->min('value');
-                                                $min = $collection->min('value');
-                                                $media = ($max + $min)/ 2.000;
-                                            @endphp
-                                            {{$collection->avg('value')}}
+                                            @if($collection->count() >= 1)
+                                                @if(in_array('value',$att_fillables))
+                                                    {{$collection->avg('value')}}
+                                                @elseif (in_array('uv_raw',$att_fillables))
+                                                    {{$collection->avg('uv_raw')}}
+                                                @elseif (in_array('average',$att_fillables))
+                                                    {{$collection->avg('average')}}
+                                                @endif
+                                            @endif
                                         </strong>
                                     </p>
                                 </div>
@@ -133,25 +196,29 @@
                         <table class="table table-bordered table-dark">
                             <head>
                                 <tr>
-                                    <td>
-                                        Timestamp
-                                    </td>
-
-                                    <td>
-                                        Valor
-                                    </td>
+                                    @if($collection->count() >= 1)
+                                        @foreach($att_fillables as $value)
+                                            <td>
+                                                {{$value}}
+                                            </td>
+                                        @endforeach
+                                    @endif
                                 </tr>
                             </head>
+
                             <tbody>
                                 @foreach($collection as $ele)
                                     <tr>
-                                        <td>
-                                            {{$ele->created_at}}
-                                        </td>
+                                        @php
+                                        $x = $ele->getFillable();
+                                        @endphp
 
-                                        <td>
-                                            {{$ele->value}}
-                                        </td>
+                                        @foreach($att_fillables as $name)
+                                            <td>
+                                                {{$ele->$name}}
+                                            </td>
+                                        @endforeach
+
                                     </tr>
                                 @endforeach
                             </tbody>
