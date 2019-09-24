8b17fec6721314142d96d63ddf3f3d0b734063ac
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-09-24 17:33:08 +0200

    Creo migración para almacenar datos del viento

---
+++ database/migrations/2019_09_24_000001_create_meteorology_winter_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeteorologyWinterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meteorology_winter', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('speed', 14, 4);
+            $table->decimal('average', 14, 4);
+            $table->decimal('min', 14, 4);
+            $table->decimal('max', 14, 4);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meteorology_winter');
+    }
+}
