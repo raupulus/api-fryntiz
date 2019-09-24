a5c8406d2469791565f2e49b791c0d2adb4f6bd2
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-09-24 14:17:48 +0200

    Creo controlador para rayos UV personalizando características propias

--- app/Api/WeatherStation/Controllers/UvController.php
+++ app/Api/WeatherStation/Controllers/UvController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+use function response;
+
 class UvController extends BaseWheaterStationController
 {
     protected $model = '\App\Uv';
@@ -13,11 +16,29 @@ class UvController extends BaseWheaterStationController
      *
      * @return mixed
      */
-    public function addValidate($request)
+    public function addValidate($data)
     {
-        return $request->validate([
+        $validator = Validator::make($data, [
             'uv_raw' => 'required|numeric',
             'risk_level' => 'nullable|string',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json('Error al validar datos de entrada', 500);
+        }
+
+        return $validator->validate();
+    }
+
+    /**
+     * Devuelve todos los elementos del modelo.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function all()
+    {
+        $model = $this->model::whereNotNull('uv_raw')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        return response()->json($model);
     }
 }
