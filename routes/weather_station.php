390a0a53326925b7c7e74cc752d9ad0c69f623a3
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-09-24 17:38:20 +0200

    Corrijo ruta

--- routes/weather_station.php
+++ routes/weather_station.php
@@ -73,10 +73,10 @@ Route::group([
         Route::post('/uv/add-json', 'UvController@addJson');
 
         ## Añadir nuevo registro de viento.
-        Route::post('/uv/add', 'WinterController@add');
+        Route::post('/winter/add', 'WinterController@add');
 
         ## Añadir nuevos registros de viento por lotes JSON
-        Route::post('/uv/add-json', 'WinterController@addJson');
+        Route::post('/winter/add-json', 'WinterController@addJson');
     }
 );
 
