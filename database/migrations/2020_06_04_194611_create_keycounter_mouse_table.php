90a3d49bdbe14c61e301a5b633ee34b921fae35e
Raúl Caro Pastorino <dev@fryntiz.es>
2020-06-04 22:36:44 +0200

    Relleno contenido para la migración de la tabla del contador de pulsaciones para el ratón

--- database/migrations/2020_06_04_194611_create_keycounter_mouse_table.php
+++ database/migrations/2020_06_04_194611_create_keycounter_mouse_table.php
@@ -14,7 +14,24 @@ class CreateKeycounterMouseTable extends Migration
     public function up()
     {
         Schema::create('keycounter_mouse', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
             $table->bigIncrements('id');
+            $table->timestamp('start_at')
+                ->comment('Momento de iniciar la racha');
+            $table->timestamp('end_at')
+                ->comment('Momento del final de la racha');
+            $table->bigInteger('pulsations')
+                ->comment('Cantidad de pulsaciones total de la racha');
+            $table->bigInteger('pulsations_special_keys')
+                ->comment('Cantidad de pulsaciones para teclas especiales total de la racha');
+            $table->decimal('pulsation_average', 15, 5)
+                ->comment('Velocidad media de pulsaciones para la racha');
+            $table->bigInteger('score')
+                ->comment('Puntuación conseguida en esta racha');
+            $table->bigInteger('weekday')
+                ->comment('Día de la semana (0 es domingo)');
             $table->timestamps();
         });
     }
