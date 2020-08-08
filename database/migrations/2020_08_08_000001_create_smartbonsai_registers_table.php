ac5c411e2c562f3ce5d0f9e5902dbd3f61d57bd1
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-09 01:20:26 +0200

    Añado atributos a la tabla para los registros de las plantas

--- database/migrations/2020_08_08_000001_create_smartbonsai_registers_table.php
+++ database/migrations/2020_08_08_000001_create_smartbonsai_registers_table.php
@@ -18,6 +18,28 @@ class CreateSmartbonsai_registersTable extends Migration
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('smartbonsai_plant_id');
+            $table->foreign('smartbonsai_plant_id')
+                ->references('id')->on('smartbonsai_plants')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('uv')
+                ->comment('Cantidad rayos UV en el ambiente');
+            $table->decimal('temperature', 13, 2)
+                ->comment('Cantidad de temperatura en el ambiente');
+            $table->decimal('humidity', 13, 2)
+                ->comment('Cantidad de humedad en el ambiente');
+            $table->decimal('soil_humidity', 13, 2)
+                ->comment('Humedad del suelo');
+            $table->integer('full_water_tank')
+                ->comment('Indica si hay agua en el tanque de agua');
+            $table->boolean('waterpump_enabled')
+                ->default(false)
+                ->comment('Indica si se ha activado la bomba de agua');
+            $table->boolean('vaporizer_enabled')
+                ->default(false)
+                ->comment('Indica si se ha activado el vaporizador');
+            $table->timestamp('created_at');
         });
     }
 
