346a8ebd41dfdc089ff832d2f8a793dcb449d7ca
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-11 15:56:25 +0200

    Creo modelo para registro de plantas

---
+++ app/Api/SmartPlant/Plant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\SmartPlant;
+
+/**
+ * Class Plant
+ *
+ * Representa una planta que será asociada desde los registros de lecturas
+ * subidos a la API.
+ *
+ * @package App\SmartPlant
+ */
+class Plant
+{
+    protected $table = 'smartbonsai_plants';
+}
