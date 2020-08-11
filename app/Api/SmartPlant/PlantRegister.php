4720f359dc81a93d592ed5498920fa7e153ca4c0
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-11 15:56:41 +0200

    Creo modelo para registrar lecturas de plantas

---
+++ app/Api/SmartPlant/PlantRegister.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\SmartPlant;
+
+/**
+ * Class Plant
+ *
+ * Representa los registros de sensores para las lecturas tomadas a las
+ * plantas asociadas en cada momento.
+ *
+ * @package App\SmartPlant
+ */
+class PlantRegister
+{
+    protected $table = 'smartbonsai_registers';
+}
