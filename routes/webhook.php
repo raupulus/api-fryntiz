0a67d5119fdfde21fb3682c1ceec3116f6ad8813
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-15 00:31:50 +0200

    Añado espacio de ruta a webhooks

--- routes/webhook.php
+++ routes/webhook.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function () {
+Route::group(['prefix' => '/webhook'], function () {
     Route::any('/api-deploy', 'Webhook\GitlabWebhookController@apiDeploy')
         ->name('webhook-api-deploy');
     Route::any('/api-notification', 'Webhook\GitlabWebhook@apiNotification')
