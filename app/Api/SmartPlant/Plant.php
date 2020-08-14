bade08437380fbe6a6eb12b13de8fb5d625a1446
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-14 22:55:52 +0200

    Creo relacion en las plantas con sus registros y creo función para obtener los 100 últimos registros

--- app/Api/SmartPlant/Plant.php
+++ app/Api/SmartPlant/Plant.php
@@ -13,4 +13,22 @@ namespace App\SmartPlant;
 class Plant
 {
     protected $table = 'smartbonsai_plants';
+
+    /**
+     * Relación con todos los registros de lecturas asociados a una planta.
+     * @return mixed
+     */
+    public function registers()
+    {
+        return $this->hasMany(PlantRegister::class, 'smartbonsai_plant_id', 'id');
+    }
+
+    public function last100registers()
+    {
+        return PlantRegister::where('smartbonsai_plant_id', $this->id)
+            ->whereNotNull('soil_humidity')
+            ->orderBy('created_at', 'DESC')
+            ->limit(100)
+            ->get();
+    }
 }
