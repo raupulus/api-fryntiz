3f219358bf75ff77b0dd00027d122ba2dd4af49a
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-30 01:33:03 +0200

    Añado al modelo para el ratón los atributos fillables

--- app/Api/KeyCounter/Mouse.php
+++ app/Api/KeyCounter/Mouse.php
@@ -5,4 +5,19 @@ namespace App\Keycounter;
 class Mouse extends Keyboard
 {
     protected $table = 'keycounter_mouse';
+
+    protected $fillable = [
+        'start_at',
+        'end_at',
+        'duration',
+        'clicks_left',
+        'clicks_right',
+        'clicks_middle',
+        'total_clicks',
+        'clicks_average',
+        'weekday',
+        'device_id',
+        'device_name',
+        'created_at'
+    ];
 }
