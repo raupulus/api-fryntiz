db62e666decd6f1989ca286cce6ddf9dd2bf70b5
Raúl Caro Pastorino <dev@fryntiz.es>
2021-03-02 22:19:12 +0100

    Add AirFlight Controller

---
+++ app/Api/AirFlight/Controllers/AirFlightController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Api\AirFlight\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class AirFlightController
+ *
+ * @package App\Http\Controllers\AirFlight
+ */
+class AirFlightController extends Controller
+{
+
+}
