5d771a57b9a1963454908d520f054969fecb5d0c
Raúl Caro Pastorino <dev@fryntiz.es>
2021-03-23 00:04:03 +0100

    Add Airplane model fillable attributes

--- app/Api/AirFlight/AirFlightAirPlane.php
+++ app/Api/AirFlight/AirFlightAirPlane.php
@@ -14,4 +14,11 @@ use Illuminate\Database\Eloquent\Model;
 class AirFlightAirPlane extends Model
 {
     protected $table = 'airflight_airplane';
+
+    protected $fillable = [
+        'icao',
+        'category',
+        'seen_last_at',
+        'seen_first_at'
+    ];
 }
