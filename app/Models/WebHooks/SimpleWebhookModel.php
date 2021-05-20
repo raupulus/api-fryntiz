7a0dc7589612d6902170149ee49ea605c84e7892
Raúl Caro Pastorino <dev@fryntiz.es>
2021-05-20 20:04:03 +0200

    Add Gitlab Webhook model and controller

---
+++ app/Models/WebHooks/SimpleWebhookModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\WebHooks;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SimpleWebhookModel
+ *
+ * @package App\Models\WebHooks
+ */
+class SimpleWebhookModel extends Model
+{
+    //
+}
