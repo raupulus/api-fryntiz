118f01b934854f7e26bf4970a23c31788e7f22d1
Raúl Caro Pastorino <dev@fryntiz.es>
2021-05-24 01:04:08 +0200

    extract functions to module

--- resources/views/layouts/footer_meta.blade.php
+++ resources/views/layouts/footer_meta.blade.php
@@ -1,9 +1,6 @@
 <script src="{{ mix('js/app.js') }}"></script>
 
 
-{{-- Mis funciones generales --}}
-<script src="{{ mix('js/functions.js') }}"></script>
-
 {{-- Scripts que serán reemplazados por algunas páginas --}}
 @section('footer-js-custom')
     <script src="{{ mix('js/scripts.js') }}"></script>
