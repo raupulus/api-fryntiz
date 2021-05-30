2fc4029ca2d18dcb3cea182d20ab570819b70d78
Raúl Caro Pastorino <dev@fryntiz.es>
2021-05-31 00:30:56 +0200

    Created helper for centralized menu generation

---
+++ app/Helpers/MenuHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Class MenuHelper
+ *
+ * Helper para ayudar a generar menús o devolver menús usados en distintos
+ * lugares de la aplicación centralizando su mantenimiento.
+ *
+ */
+class MenuHelper
+{
+    /**
+     * Devuelve las rutas para acceder al listado principal de cada api.
+     *
+     * @return array[]
+     */
+    public static function getApiRoutesIndex()
+    {
+        $path = request()->path();
+
+        return [
+            'weatherstation' => [
+                'route' => route('weather_station.index'),
+                'name' => 'Weather Station',
+                'selected' => $path == 'weatherstation',
+            ],
+            'keycounter' => [
+                'route' => route('keycounter.index'),
+                'name' => 'Keycounter',
+                'selected' => $path == 'keycounter',
+            ],
+            'airflight' => [
+                'route' => route('airflight.index'),
+                'name' => 'Airflight',
+                'selected' => $path == 'airflight',
+            ],
+            'smartplant' => [
+                'route' => route('smartplant.index'),
+                'name' => 'Smart Plant',
+                'selected' => $path == 'smartplant',
+            ],
+        ];
+    }
+
+    /**
+     * Devuelve un array con las rutas hacia la documentación de las Apis.
+     *
+     * @return array
+     */
+    public static function getApiRoutesDocs()
+    {
+        return [];
+    }
+}
