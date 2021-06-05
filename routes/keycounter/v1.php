e4b1ed3e99c711cfb2aca2a5ceab28d5aec4aaad
Raúl Caro Pastorino <dev@fryntiz.es>
2021-06-05 23:04:10 +0200

    Restructured keycounter routes

--- routes/keycounter/v1.php
+++ routes/keycounter/v1.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Archivo de rutas para la api para la pulsación de teclas y ratón con él
- * sufijo /keycounter/*
+ * sufijo /api/keycounter/v1/*
  */
 
 use Illuminate\Support\Facades\Route;
@@ -9,19 +9,12 @@ use Illuminate\Support\Facades\Route;
 ######################################################
 ##                    Pública
 ######################################################
-Route::get('/', 'App\Http\Controllers\KeyCounter\KeyCounterController@index')->name('keycounter.index');
 
-Route::get('/test', function () {
-    return 'Ruta de prueba accesible desde' . url('test');
-});
 
 ######################################################
 ##                    Privada
 ######################################################
-Route::group([
-    'prefix' => '/',
-    'middleware' => ['auth:sanctum']
-], function () {
+Route::group(['middleware' => ['auth:sanctum']], function () {
     ## Añadir nuevo registro de pulsaciones para el teclado.
     Route::post('/keyboard/add', 'App\Http\Controllers\Api\KeyCounter\KeyboardController@add');
 
@@ -34,4 +27,3 @@ Route::group([
     ## Añadir nuevos registros de pulsaciones para el teclado  por lotes JSON
     Route::post('/mouse/add-json', 'App\Http\Controllers\Api\KeyCounter\MouseController@addJson');
 });
-
---
+++ routes/keycounter/web.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Archivo de rutas para la api para la pulsación de teclas y ratón con él
+ * sufijo /keycounter/*
+ */
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\KeyCounter\KeyCounterController@index')->name('keycounter.index');
