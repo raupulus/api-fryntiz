305a4b9c61657aff456d02cd8368ab6b6e213a6b
Raúl Caro Pastorino <dev@fryntiz.es>
2022-01-05 00:10:52 +0100

    Added model methods

--- app/Models/CV/CurriculumService.php
+++ app/Models/CV/CurriculumService.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\CV;
 
+use App\Models\File;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -12,4 +13,69 @@ use Illuminate\Database\Eloquent\Model;
 class CurriculumService extends Model
 {
     protected $table = 'cv_services';
+
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Relaciona con el curriculum.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function curriculum()
+    {
+        return $this->belongsTo(Curriculum::class, 'curriculum_id', 'id');
+    }
+
+    /**
+     * Relación con la imagen asociada al curriculum.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function image()
+    {
+        return $this->belongsTo(File::class, 'image_id', 'id');
+    }
+
+    /**
+     * Devuelve la ruta hacia la foto asociada al curriculum.
+     *
+     * @return string
+     */
+    public function getUrlImageAttribute()
+    {
+        return $this->image ? $this->image->url : File::urlDefaultImage('large');
+    }
+
+    /**
+     * Devuelve el thumbnail de la imagen asociada.
+     *
+     * @param $size
+     *
+     * @return mixed
+     */
+    public function urlThumbnail($size = 'medium')
+    {
+        if ($this->image) {
+            return $this->image->thumbnail($size);
+        }
+
+        return File::urlDefaultImage($size);
+    }
+
+    /**
+     * Elimina de forma segura un repositorio y los datos asociados.
+     *
+     * @return bool
+     */
+    public function safeDelete()
+    {
+        ## Elimino la imagen asociada al curriculum y todas las miniaturas.
+        if ($this->image) {
+            $this->image->safeDelete();
+        }
+
+        return $this->delete();
+    }
 }
