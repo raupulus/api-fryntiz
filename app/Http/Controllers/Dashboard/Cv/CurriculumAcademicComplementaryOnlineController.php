f628803a041576d196df5106437ad971f0e14067
Raúl Caro Pastorino <dev@fryntiz.es>
2022-01-07 23:20:52 +0100

    All dashboard controllers curriculums created base

---
+++ app/Http/Controllers/Dashboard/Cv/CurriculumAcademicComplementaryOnlineController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Cv;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CurriculumAcademicComplementaryOnlineController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Dashboard/Cv/CurriculumExperienceAdditionalController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Cv;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CurriculumExperienceAdditionalController extends Controller
+{
+    //
+}
