40435c5744981c940beaeecae181afea2038b077
Raúl Caro Pastorino <dev@fryntiz.es>
2022-01-30 13:17:49 +0100

    Created migration for user referred

--- database/migrations/2019_07_04_232015_create_referred_users_table.php
+++ database/migrations/2019_07_04_232015_create_referred_users_table.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Migrations\Migration;
 /**
  * Class CreateReferredPlatformsTable
  *
- * Tabla para almacenar las plataformas de referidos disponibles.
+ * Tabla para almacenar los usuarios asociados a plataformas de referidos.
  */
-class CreateReferredPlatformsTable extends Migration
+class CreateReferredUsersTable extends Migration
 {
-    private $tableName = 'referred_platforms';
+    private $tableName = 'referred_users';
 
     /**
      * Run the migrations.
@@ -26,29 +26,32 @@ class CreateReferredPlatformsTable extends Migration
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('image_id')
-                ->nullable()
-                ->comment('Relación con la imagen asociada');
-            $table->string('name', '255')
+            $table->unsignedBigInteger('user_id')
+                ->comment('Relación con el usuario');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->unsignedBigInteger('referred_platform_id')
+                ->comment('Relación con la plataforma de referido');
+            $table->foreign('referred_platform_id')
+                ->references('id')->on('referred_platforms')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->string('id_affiliated', '255')
                 ->unique()
-                ->comment('Nombre de la plataforma para afiliados.');
-            $table->string('slug', '255')
+                ->comment('El id de afiliado dentro de la plataforma.');
+            $table->string('token', '255')
                 ->unique()
-                ->comment('Nombre interno del role.');
-            $table->text('description')
+                ->comment('Token para conectar con la plataforma.');
+            $table->date('start_at')
                 ->nullable()
-                ->comment('Descripción de la plataforma.');
-            $table->string('url', '255')
-                ->nullable()
-                ->comment('Enlace a la página principal.');
-            $table->string('url_panel')
-                ->nullable()
-                ->comment('Enlace al panel de control para referidos.');
+                ->comment('Fecha de inicio de la afiliación.');
             $table->timestamps();
             $table->softDeletes();
         });
 
-        $comment = 'Tabla para almacenar las plataformas de referidos.';
+        $comment = 'Tabla para almacenar los usuarios asociados a plataformas de referidos.';
 
         DB::statement("COMMENT ON TABLE {$this->tableName} IS '{$comment}'");
     }
@@ -61,7 +64,8 @@ class CreateReferredPlatformsTable extends Migration
     public function down()
     {
         Schema::dropIfExists($this->tableName, function (Blueprint $table) {
-            $table->dropForeign(['image_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['referred_platform_id']);
         });
     }
 }
