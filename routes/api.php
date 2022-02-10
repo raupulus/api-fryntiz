6417d1ce42ade61a2a96a8e15f58dee0ffbd500b
Raúl Caro Pastorino <dev@fryntiz.es>
2022-02-10 22:56:38 +0100

    Added user data and login routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\User\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,33 +26,41 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 ##                   Autenticación
 ######################################################
 
-Route::group([
-    'prefix' => 'auth'
-],
-    function () {
-        Route::post('/login', [LoginController::class, 'login']);
-        Route::post('signup', [RegisterController::class, 'register']);
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [LoginController::class, 'login'])->middleware(\Illuminate\Session\Middleware\StartSession::class);
+    Route::post('signup', [RegisterController::class, 'register']);
 
-//Route::post('/register', [AuthController::class, 'register']);
-//Route::post('/login', [AuthController::class, 'login']);
+    //Route::post('/register', [AuthController::class, 'register']);
+    //Route::post('/login', [AuthController::class, 'login']);
 
-        /*
-        Route::post('/tokens/create', function (Request $request) {
-            $token = $request->user()->createToken($request->token_name);
+    /*
+    Route::post('/tokens/create', function (Request $request) {
+        $token = $request->user()->createToken($request->token_name);
 
-            return ['token' => $token->plainTextToken];
-        });
-        */
+        return ['token' => $token->plainTextToken];
+    });
+    */
 
-        Route::group([
-            'middleware' => 'auth:sanctum'
-        ], function() {
-            Route::get('/logout', 'AuthController@logout');
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::get('/logout', 'AuthController@logout');
+    });
+});
+
+######################################################
+##                   Usuarios
+######################################################
+Route::group(['prefix' => 'user','middleware' => 'auth:sanctum'], function () {
+    ## Datos del perfil para el usuario logueado.
+    Route::get('/profile', [UserProfileController::class, 'myProfile']);
+
+    ## Datos del perfil de un usuario.
+    Route::get('/profile/{user_id}', [UserProfileController::class, 'show']);
+});
 
 
-            Route::get('/user', function () {
-                return response()->json(auth()->user());
-            });
-        });
-    }
-);
+/**
+ * Ruta por defecto cuando no se encuentra una petición.
+ */
+Route::fallback(function(){
+    return response()->json(['message' => 'Page Not Found'], 404);
+});
