600632df508babcbbe352b0b655231893035b4e3
Raúl Caro Pastorino <dev@fryntiz.es>
2022-02-10 22:59:02 +0100

    Create register controller file

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * Class RegisterController
+ *
+ * @package App\Http\Controllers\Api\Auth
+ */
+class RegisterController extends Controller
+{
+    //
+}
