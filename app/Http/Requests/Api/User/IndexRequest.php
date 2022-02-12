e90380215e7eba38c301cc4efa21c543a08c3b11
Raúl Caro Pastorino <dev@fryntiz.es>
2022-02-12 19:59:24 +0100

    Created request for show all users

---
+++ app/Http/Requests/Api/User/IndexRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api\User;
+
+use App\Http\Requests\Api\BaseFormRequest;
+
+/**
+ * Class IndexRequest
+ */
+class IndexRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
