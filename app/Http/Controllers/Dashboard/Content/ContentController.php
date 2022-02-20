936e2fcc16f06712364869ad51bf1a128a28e3ab
Raúl Caro Pastorino <dev@fryntiz.es>
2022-02-20 01:09:11 +0100

    Created dashboard content controllers

---
+++ app/Http/Controllers/Dashboard/Content/ContentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Content;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class ContentAvailableCategoryController
+ * @package App\Http\Controllers\Dashboard\Content
+ */
+class ContentController extends Controller
+{
+    //
+}
