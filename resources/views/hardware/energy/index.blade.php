324f27ba1032e0321e64e54e7affb9ebd0c1ce10
Raúl Caro Pastorino <dev@fryntiz.es>
2022-03-08 23:43:31 +0100

    Implement hardware list and today-historical resume

--- resources/views/hardware/energy/index.blade.php
+++ resources/views/hardware/energy/index.blade.php
@@ -57,7 +57,8 @@
                             </h6>
 
                             <span class="text-xl font-semibold">
-                                0kwh
+                                {{$hardwareGeneratorHistorical->sum('cumulative_power_generation')}}
+                                kwh
                             </span>
 
                             <span
@@ -94,10 +95,14 @@
                             >
                                 Consumido
                             </h6>
-                            <span class="text-xl font-semibold">50,021</span>
+                            <span class="text-xl font-semibold">
+                                {{$hardwareGeneratorHistorical->sum('cumulative_power_consumption')}}
+                                kwh
+                            </span>
+
                             <span
                                 class="inline-block px-2 py-px ml-2 text-xs text-green-500 bg-green-100 rounded-md">
-                      +2.6%
+                      +0.0%
                     </span>
                         </div>
                         <div>
@@ -130,7 +135,9 @@
                                 Actual
                             </h6>
                             <span class="text-xl font-semibold">
-                                0kwh
+                                {{$hardwareGeneratorToday->sum
+                                ('cumulative_power_generation')}}
+                                kwh
                             </span>
                             <span
                                 class="inline-block px-2 py-px ml-2 text-xs text-green-500 bg-green-100 rounded-md">
@@ -167,7 +174,9 @@
                                 Hoy
                             </h6>
                             <span class="text-xl font-semibold">
-                                0kwh
+                                {{$hardwareGeneratorToday->sum
+                                ('cumulative_power_consumption')}}
+                                kwh
                             </span>
                             <span
                                 class="inline-block px-2 py-px ml-2 text-xs text-green-500 bg-green-100 rounded-md">
@@ -198,8 +207,6 @@
         </section>
 
 
-
-
         {{-- Listado de dispositivos --}}
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
@@ -214,159 +221,42 @@
                 </div>
 
                 <div class="flex flex-wrap -m-4">
-                    <div class="p-4 lg:w-1/2">
-                        <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
 
-                            <img alt="team"
-                                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                                 src="https://dummyimage.com/200x200">
+                    @foreach($hardwares as $hardware)
 
-                            <div class="flex-grow sm:pl-8">
-                                <h2 class="title-font font-medium text-lg text-gray-900">
-                                    Dispositivo 1
-                                </h2>
-                                <h3 class="text-gray-500 mb-3">
-                                    UI Developer
-                                </h3>
+                        <div class="p-4 lg:w-1/2">
+                            <div
+                                class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
 
-                                <p class="mb-4">
-                                    DIY tote bag drinking vinegar
-                                    cronut adaptogen squid fanny pack
-                                    vaporware.
-                                </p>
+                                <img alt="team"
+                                     class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                                     src="https://dummyimage.com/200x200">
 
-                                <span class="inline-flex">
+                                <div class="flex-grow sm:pl-8">
+                                    <h2 class="title-font font-medium text-lg text-gray-900">
+                                        {{$hardware->name}}
+                                    </h2>
+                                    <h3 class="text-gray-500 mb-3">
+                                        {{$hardware->version}}
+                                    </h3>
 
-            </span>
+                                    <p class="mb-4">
+                                        {{$hardware->description}}
+                                    </p>
+
+                                    <p class="mb-4">
+                                        !Gráfico consumo-generación!
+                                    </p>
+
+                                    <span class="inline-flex">
+
+                                </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="p-4 lg:w-1/2">
-                        <div
-                            class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                            <img alt="team"
-                                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                                 src="https://dummyimage.com/201x201">
-                            <div class="flex-grow sm:pl-8">
-                                <h2 class="title-font font-medium text-lg text-gray-900">
-                                    Alper Kamu</h2>
-                                <h3 class="text-gray-500 mb-3">Designer</h3>
-                                <p class="mb-4">DIY tote bag drinking vinegar
-                                    cronut adaptogen squid fanny pack
-                                    vaporware.</p>
-                                <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="p-4 lg:w-1/2">
-                        <div
-                            class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                            <img alt="team"
-                                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                                 src="https://dummyimage.com/204x204">
-                            <div class="flex-grow sm:pl-8">
-                                <h2 class="title-font font-medium text-lg text-gray-900">
-                                    Atticus Finch</h2>
-                                <h3 class="text-gray-500 mb-3">UI Developer</h3>
-                                <p class="mb-4">DIY tote bag drinking vinegar
-                                    cronut adaptogen squid fanny pack
-                                    vaporware.</p>
-                                <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="p-4 lg:w-1/2">
-                        <div
-                            class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                            <img alt="team"
-                                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                                 src="https://dummyimage.com/206x206">
-                            <div class="flex-grow sm:pl-8">
-                                <h2 class="title-font font-medium text-lg text-gray-900">
-                                    Henry Letham</h2>
-                                <h3 class="text-gray-500 mb-3">Designer</h3>
-                                <p class="mb-4">DIY tote bag drinking vinegar
-                                    cronut adaptogen squid fanny pack
-                                    vaporware.</p>
-                                <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                     stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-                            </div>
-                        </div>
-                    </div>
+
+                    @endforeach
+
                 </div>
             </div>
         </section>
@@ -375,7 +265,7 @@
         <section class="bg-white border-b">
             <div class="container max-w-5xl mx-auto m-4">
                 <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-                    Energía (Generadores y Consumo)
+                    Section name
                 </h1>
 
                 <div class="w-full py-3 px-6 text-center">
@@ -402,12 +292,6 @@
 
 
 
-
-    TEST
-
-    https://tailwind-elements.com/docs/standard/components/charts/
-    https://tailwindcomponents.com/
-    https://github.com/aniftyco/awesome-tailwindcss
 
     @include('hardware.energy.layouts._generators')
     @include('hardware.energy.layouts._loads')
