6a1a53e3c8c111f9c1672ad204d6587bb92b9bf9
Raúl Caro Pastorino <dev@fryntiz.es>
2022-04-12 00:13:11 +0200

    Created user controller with general list

--- app/Http/Controllers/Dashboard/Users/UserController.php
+++ app/Http/Controllers/Dashboard/Users/UserController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers\Dashboard\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
+/**
+ * Class UserController
+ */
 class UserController extends Controller
 {
     public function index()
     {
-        return view('dashboard.users.index');
+        ## Usuarios Activos.
+        $n_usersActive = User::countActive();
+
+        ## Usuarios nuevos de este mes
+        $n_users_this_month = User::countNewInThisMonth();
+
+        ## Usuarios Inactivos (SoftDelete)
+        $n_usersInactive = User::countInactive();
+
+        $n_users = $n_usersActive + $n_usersInactive;
+
+        return view('dashboard.users.index')->with([
+            'users' => User::all(), // TEMPORAL, traer por ajax
+            'n_users' => $n_users,
+            //'usersInactive' => $usersInactive,
+            'n_usersActive' => $n_usersActive,
+            'n_usersInactive' => $n_usersInactive,
+            'n_users_this_month' => $n_users_this_month,
+        ]);
     }
 }
