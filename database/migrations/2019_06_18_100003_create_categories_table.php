e7f2fed14c7de49aa89cb7e914aa72fddaa5f05f
Raúl Caro Pastorino <dev@fryntiz.es>
2022-05-19 19:22:28 +0200

    Creo migración para categorías

---
+++ database/migrations/2019_06_18_100003_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->id();
+            $table->string('name', 255)->unique();
+            $table->string('slug', 255)->unique();
+            $table->string('description', 511)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
