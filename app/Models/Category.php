1fab3aab773fa362985c49db581cabb9601991b4
Raúl Caro Pastorino <dev@fryntiz.es>
2022-06-02 18:59:33 +0200

    Added fillable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\BaseModels\BaseModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Category extends BaseModel
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'description'];
+
 }
