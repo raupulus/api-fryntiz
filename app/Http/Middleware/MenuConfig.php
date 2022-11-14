e6ec29de7718d6f3a656aa7170fc879936643a6e
Raúl Caro Pastorino <raul@fryntiz.dev>
2022-11-14 19:27:56 +0100

    Enhanced Dynamic menu

--- app/Http/Middleware/MenuConfig.php
+++ app/Http/Middleware/MenuConfig.php
@@ -23,94 +23,50 @@ class MenuConfig
     {
 
         $contentMenu = config('adminlte_extra.menu.content');
-        $contentCreateSubmenu = [
-            'text' => 'Nuevo',
-            'icon' => 'fas fa-fw fa-users',
-            'submenu' => []
-        ];
-
-
-
-        /**
-        foreach (Platform::all() as $platform) {
-            $contentCreateSubmenu['submenu'][] = [
-                'text' => Str::limit($platform->title, 22, '...'),
-                'url' => route('dashboard.content.create', $platform->id),
-                'icon' => 'fas fa-plus',
-            ];
-        }
-         */
-
 
         // TODO -> Añadir esto a caché de 1 hora
         // Intentando generar menú dinámico
 
-        $contentTypes = ContentAvailableType::all();
-
         foreach (Platform::all() as $platform) {
             $newPlatformMenu = [
                 'text' => Str::limit($platform->title, 22, '...'),
-                'icon' => 'fas fa-fw fa-users',
-                'submenu' => []
+                'icon' => 'fas fa-fw fa-globe',
+                'submenu' => [
+                    [
+                        'text' => 'Listado',
+                        'icon' => 'fas fa-eye',
+                        'url' => route('dashboard.content.create', $platform->id),
+
+                        // TODO: Todas las entradas con errores de validación al comprobar autoría
+                        //'label' => 4,
+                        //'label_color' => 'warning',
+                    ],
+                    [
+                        'text' => 'Nuevo',
+                        'icon' => 'fas fa-plus',
+                        'url' => route('dashboard.content.create', $platform->id),
+
+                        // TODO: Dinamizar con las entradas nuevas sin publicar
+                        //'label' => 7,
+                        //'label_color' => 'success',
+                    ]
+                ]
             ];
-
-
-            ## Menú para Crear
-            $createMenu = [
-                'text' => 'Nuevo',
-                'icon' => 'fas fa-plus',
-                'submenu' => []
-            ];
-
-            ## Cada tipo de contenido
-            foreach ($contentTypes as $contentType) {
-                $createMenu['submenu'][] = [
-                    'text' => Str::limit($contentType->name, 22, '...'),
-                    'url' => route('dashboard.content.create', $platform->id),
-                    'icon' => 'fas fa-plus',
-                ];
-            }
-
-            $newPlatformMenu['submenu'][] = $createMenu;
 
             $contentMenu[] = $newPlatformMenu;
         }
 
-
-
-
-
-
-        //$contentMenu[] = $contentCreateSubmenu;
-
-
-
         $tmp = config('adminlte_extra.menu.tmp');
 
         $menus = array_merge($contentMenu, $tmp);
-
 
         Event::listen(BuildingMenu::class, function (BuildingMenu $event) use ($menus) {
 
             foreach ($menus as $item) {
                 $event->menu->add($item);
             }
-            //$event->menu->add($contentMenu);
+
         });
-
-
-
-
-        /*
-
-
-        $contentRoutes = \App\Models\Platform::pluck('id')->map(function
-        ($platformId) {
-            return route('dashboard.content.create', $platformId);
-        })->toArray();
-
-        $menu['content'] = $contentRoutes;
-*/
 
 
         return $next($request);
--- config/adminlte_extra/menu/content.php
+++ config/adminlte_extra/menu/content.php
@@ -6,35 +6,7 @@ return [
     [
         'text' => 'Todos',
         'route' => 'dashboard.content.index',
-        'icon' => 'far fa-fw fa-file',
+        'icon' => 'far fa-fw fa-eye',
     ],
-
-    [
-        'text' => 'Categorías',
-        'url' => '#',
-        'icon' => 'far fa-fw fa-file',
-    ],
-
-    // TODO → Dinamizar por tipo de contenido
-    [
-        'text' => 'blog',
-        'url' => 'admin/blog',
-        'can' => 'manage-blog', // Esto asigna permiso para ver la opción
-    ],
-    [
-        'text' => 'pages',
-        'url' => '#',
-        'icon' => 'far fa-fw fa-file',
-        'label' => 4,
-        'label_color' => 'success',
-    ],
-    [
-        'text' => 'noticias',
-        'url' => '#',
-        'icon' => 'far fa-fw fa-file',
-        'label' => 4,
-        'label_color' => 'success',
-    ],
-
 ];
 
