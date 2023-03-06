192ef8c982f58f7b5b5973718821dd836790441b
Raúl Caro Pastorino <raul@fryntiz.dev>
2023-03-06 02:03:26 +0100

    Fix prevent user create

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1/auth'], function () {
     ## Ruta para hacer login.
     Route::post('/login', [LoginController::class, 'login'])
-        ->middleware(\Illuminate\Session\Middleware\StartSession::class)
+        //->middleware(\Illuminate\Session\Middleware\StartSession::class)
         ->name('api.v1.auth.login');
 
     ## Ruta para crear un nuevo registro de usuario.
