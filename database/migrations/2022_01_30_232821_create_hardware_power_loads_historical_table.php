47775e2194980ab72641e5c744abc3c520dbd4a7
Raúl Caro Pastorino <raul@fryntiz.dev>
2023-03-21 01:21:56 +0100

    Fix hardware load historical table migration

--- database/migrations/2022_01_30_232821_create_hardware_power_loads_historical_table.php
+++ database/migrations/2022_01_30_232821_create_hardware_power_loads_historical_table.php
@@ -36,57 +36,53 @@ class CreateHardwarePowerLoadsHistoricalTable extends Migration
                 ->onDelete('CASCADE');
             $table->integer('fan_min')
                 ->nullable()
-                ->default(0)
                 ->comment('Velocidad mínima de ventilador (rpm)');
             $table->integer('fan_max')
                 ->nullable()
-                ->default(0)
                 ->comment('Velocidad máxima de ventilador (rpm)');
-            $table->decimal('temperature_min', 10, 2)
+            $table->decimal('temperature_min', 6, 3)
                 ->nullable()
                 ->default(0)
                 ->comment('Temperatura mínima (°C)');
-            $table->decimal('temperature_max', 10, 2)
+            $table->decimal('temperature_max', 6, 3)
                 ->nullable()
                 ->default(0)
                 ->comment('Temperatura máxima (°C)');
-            $table->decimal('voltage_min', 10, 2)
+            $table->decimal('voltage_min', 8, 3)
                 ->nullable()
                 ->default(0)
                 ->comment('Volts mínimo (V)');
-            $table->decimal('voltage_max', 10, 2)
+            $table->decimal('voltage_max', 8, 3)
                 ->nullable()
                 ->default(0)
                 ->comment('Voltaje máximo (V)');
-            $table->decimal('battery_min', 10, 2)
+            $table->decimal('battery_min', 8, 3)
                 ->nullable()
-                ->default(0)
                 ->comment('Voltaje mínimo de batería (V)');
-            $table->decimal('battery_max', 10, 2)
+            $table->decimal('battery_max', 8, 3)
                 ->nullable()
-                ->default(0)
                 ->comment('Voltaje máximo de batería (V)');
-            $table->decimal('amperage_min', 10, 2)
+            $table->decimal('amperage_min', 20, 6)
                 ->nullable()
                 ->default(0)
                 ->comment('Amperaje mínimo (A)');
-            $table->decimal('amperage_max', 10, 2)
+            $table->decimal('amperage_max', 20, 6)
                 ->nullable()
                 ->default(0)
                 ->comment('Amperaje máximo (A)');
-            $table->double('amperage')
+            $table->decimal('amperage', 20, 6)
                 ->nullable()
                 ->default(0)
                 ->comment('Amperaje total (A)');
-            $table->decimal('power_min', 10, 2)
+            $table->decimal('power_min', 20, 6)
                 ->nullable()
                 ->default(0)
                 ->comment('Potencia mínima (W)');
-            $table->decimal('power_max', 10, 2)
+            $table->decimal('power_max', 20, 6)
                 ->nullable()
                 ->default(0)
                 ->comment('Potencia máxima (W)');
-            $table->decimal('power', 10, 2)
+            $table->decimal('power', 20, 6)
                 ->nullable()
                 ->default(0)
                 ->comment('Potencia total (W)');
