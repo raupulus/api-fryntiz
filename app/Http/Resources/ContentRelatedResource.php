ab6e14d5e4ef79770840436096a4da6044070a5d
Raúl Caro Pastorino <raul@fryntiz.dev>
2025-08-04 04:13:16 +0200

    Created Resource for ContentRelated

--- app/Http/Resources/ContentRelatedResource.php
+++ app/Http/Resources/ContentRelatedResource.php
@@ -16,19 +16,29 @@ class ContentRelatedResource extends JsonResource
     {
         $images = null;
 
-        if ($this->image) {
+        if ($this->image_id) {
             $images = [
-                'urlImageSmall' => $this->urlImageSmall,
-                'urlImageMedium' => $this->urlImageMedium,
+                'small' => $this->urlImageSmall,
+                'medium' => $this->urlImageMedium,
             ];
         }
 
         return [
             'title' => $this->title,
+            'type' => $this->type?->slug,
             'slug' => $this->slug,
             'excerpt' => $this->excerpt,
+            'has_image' => (bool) $this->image_id,
             'images' => $images,
             'updated_at' => $this->updated_at,
+            'categories' => $this->categoriesQuery()->select(['categories.name', 'categories.slug'])->get(),
+            'subcategories' => $this->subcategoriesQuery()->select(['categories.name', 'categories.slug', 'content_categories.is_main', 'parent_id'])->get()->map(function ($subcat) {
+                $subcat->parent = $subcat->parentCategory?->slug;
+                unset($subcat->parent_id);
+                unset($subcat->parentCategory);
+
+                return $subcat;
+            }),
         ];
     }
 }
