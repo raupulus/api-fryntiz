e865b817d084f0acd54ba9c78164a1a809695d90
Raúl Caro Pastorino <raul@fryntiz.dev>
2026-05-27 05:47:47 +0200

    Add CSRF and cookie middleware to Tenant Panel

--- app/Providers/Filament/TenantPanelProvider.php
+++ app/Providers/Filament/TenantPanelProvider.php
@@ -9,6 +9,9 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
@@ -31,9 +34,12 @@ class TenantPanelProvider extends PanelProvider
                 \App\Filament\Tenant\Pages\Dashboard::class,
             ])
             ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
